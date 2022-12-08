c8972e69fff9c23bac420d1ef528cd643ee351cb
kandarp sharda <009kandarp@gmail.com>
2022-12-08 23:49:06 +0530

    commit done

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from typing import List
-
 from fastapi import Depends, FastAPI, HTTPException
+from pytz import timezone
 from sqlalchemy.orm import Session
-import os
 import operation as operation, models as models, schema as schema
 from database import SessionLocal, engine
 from typing import Any
 from fastapi import FastAPI
 import socketio
+import json
+from fastapi.middleware.cors import CORSMiddleware
+from datetime import datetime
+import uvicorn
+
 sio: Any = socketio.AsyncServer(async_mode="asgi" ,  cors_allowed_origins='*')
 socket_app = socketio.ASGIApp(sio)
 
@@ -23,59 +27,9 @@ def get_db():
     finally:
         db.close()
         
-# @app.post("/SignUp/user", response_model=schema.UserBase)
-# def create_user(user: schema.CreateUser, db: Session = Depends(get_db)):
-#     if operation.find_user(db,user.name ):
-#         raise HTTPException(status_code=400, detail="User already exist")
-#     return operation.create_user(db=db, user_data=user)
-
-# @app.post("/SignIn/user" )
-# def login_user(user :schema.Login , db: Session = Depends(get_db)):
-   
-#     data  = operation.login_user(db , user)
-#     if not data :
-#         raise HTTPException(status_code=400, detail  = "Invalid username or password")
-#     return data.id
-# @app.post("/user/foodshare")
-# def food_share(food :schema.FoodDetail , db: Session = Depends(get_db) ):
-#     data  =  operation.store_data(db  ,  food)
-#     if not data:
-#         raise HTTPException(status_code=400, detail = "Invalid argument")
-#     return  "data has been stored"
-
-# @app.get("/shared/{id}" , response_model = List[schema.UserFoodDetail])
-# def user_food_share( id:str, db: Session = Depends(get_db)):
-#     print(operation.shared_food_data(db ,  id) )
-#     # print(operation.shared_food_data(db ,  id).shared_food[0].__dict__  )
-#     return operation.shared_food_data(db ,  id)  
-
-# # app.mount("/", socket_app)
-
-# # @sio.event
-# # async def connect(sid  , env):
-# #     print("on connect")
-    
-# # # # @sio.on("")
-    
-    
 
 
 
-# if __name__=='__main__':
-#     os.system('uvicorn main:app --host 10.1.157.173  --port 9000 --reload')
-
-from typing import Any
-from urllib import response
-
-import uvicorn
-from fastapi import FastAPI
-import socketio
-from fastapi.middleware.cors import CORSMiddleware
-from pytz import timezone 
-from datetime import datetime
-import json
-sio: Any = socketio.AsyncServer(async_mode="asgi" , cors_allowed_origins='*')
-socket_app = socketio.ASGIApp(sio)
 app = FastAPI()
 
 socket_user  =   {}
@@ -99,8 +53,7 @@ async def test():
 
 @app.post("/SignUp/user")
 def create_user(user: schema.CreateUser, db: Session = Depends(get_db)):
-    print(user)
-    # print(operation.find_user(db,user.name ) ,  "er32")
+  
     if operation.find_user(db,user.email ):
         raise HTTPException(status_code=400, detail="User already exist")
     return operation.create_user(db=db, user_data=user)
@@ -109,17 +62,14 @@ def create_user(user: schema.CreateUser, db: Session = Depends(get_db)):
 def login_user(user :schema.Login , db: Session = Depends(get_db)):
    
     data  = operation.login_user(db , user)
-    # # first data
-    # print(data.__dict__)
+ 
     if not data :
         raise HTTPException(status_code=400, detail  = "Invalid username or password")
     return data.__dict__
 
 
 @app.get("/shared/{id}" , response_model = List[schema.UserFoodDetail])
-def user_food_share( id:str, db: Session = Depends(get_db)):
-    print(operation.shared_food_data(db ,  id) )
-   
+def user_food_share( id:str, db: Session = Depends(get_db)): 
     return operation.shared_food_data(db ,  id)  
 
 
@@ -145,8 +95,6 @@ async def broadcast(sid, msg):
     if msg!=False:
         data  =  operation.store_data( next(get_db())   ,  msg )
         await sio.emit("check", True if  data else False , room  = sid )
-
-
         
     food_list_data =  operation.list_food(next(get_db()) )
     food_list_data_list =   []
@@ -196,17 +144,6 @@ async def function1(sid ,msg):
 @sio.on("user_donor")
 async def function2(sid  , msg):
     await sio.emit("user_donor_data"  ,operation.giving_user_order( next(get_db()) ,  msg)  )
-    
-
-   
-    
-    
-    
-    
-    
-        
-    
-    
 
 @sio.on("disconnect")
 async def disconnect(sid):
